141ba6534ec8160e53d247f7e7f586651780ba0a
Лакатош Віталій <some.pochta1@gmail.com>
2017-06-12 07:34:06 +0300

    Зміни в моделі

---
+++ book.rb
@@ -0,0 +1,17 @@
+class Book < ApplicationRecord
+	acts_as_votable
+	#belongs_to :user
+	has_many :vidguks
+	has_many :kritics
+	#belongs_to :zhanr
+	validates :title, :auth, :zhanr, :pages_qty, :description, :image_url, :mini_description, :url_to_book, :name_of_file, :language, :rozshirennya,  :origin_url_to_book,  :presence => true
+	def self.search(search)
+		where(['title LIKE ? OR auth LIKE ? OR zhanr LIKE ? OR description LIKE ? ', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
+
+	end	
+	has_attached_file :image_url, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :image_url, content_type: /\Aimage\/.*\z/
+
+  	has_attached_file :url_to_book
+  	validates_attachment :url_to_book, content_type: { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+end
